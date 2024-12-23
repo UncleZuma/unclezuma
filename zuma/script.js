fee9b7d078800f1536f814ac96261273934a7091
UncleZuma <unclezuma4@gmail.com>
2024-12-23 02:51:49 -0800

    Add files via upload

---
+++ zuma/script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener(
+    'submit', function (e) {
+        e.preventDefault();
+        alert('Thank you for contacting me! I will get back to you shortly.');
+
+
+    }
+)
